a880cfd70b7851796f2c13f8f74adbf098fd510e
biswajit <biswajitsahoo@muvi.com>
2024-03-18 12:29:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(), 
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      // Navigator.of(context).pushReplacement(
+        // MaterialPageRoute(builder: (BuildContext context) => const NewsPage(title: 'Home Page')),
+      // );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.white, // Background color
+          image: DecorationImage(
+            image: AssetImage('assets/splash_image.png'), 
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NewsPage extends StatefulWidget {
+  @override
+  _NewsPageState createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+  List<dynamic> newsList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNews();
+  }
+
+  Future<void> fetchNews() async {
+    final String apiUrl =
+        'https://api.thenewsapi.com/v1/news/top?api_token=iHZffrc7gNpP2UqWZsF60dXx6bZUDAlyQXLKyvsF&locale=us&limit=3';
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        setState(() {
+          newsList = json.decode(response.body)['data'];
+        });
+      } else {
+        throw Exception('Failed to load news');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top News'),
+      ),
+      body: ListView.builder(
+        itemCount: newsList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(newsList[index]['title']),
+            subtitle: Text(newsList[index]['description']),
+          );
+        },
+      ),
+    );
+  }
+}
